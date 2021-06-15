a433906ff49fb4fd12fed16ad2bd7d97ec3b2264
Michele Lorusso <kirito102095@hotmail.it>
2021-06-15 16:07:32 +0200

    first commit

---
+++ database/migrations/2021_06_15_125101_create_comics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->text('poster');
+            $table->float('price', 5, 2);
+            $table->date("start_date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
